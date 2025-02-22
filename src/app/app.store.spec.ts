4718050925c0e6a18227b12f65e752c32e4d0584
RemyLabecot <remylabecot@gmail.com>
2025-02-22 20:27:57 +0100

    final prod

--- src/app/app.store.spec.ts
+++ src/app/app.store.spec.ts
@@ -3,6 +3,9 @@ import { ProjectsEnum } from './enum/projects.enum';
 import { AgentEnum } from './enum/agents.enum';
 import { TestBed, waitForAsync } from '@angular/core/testing';
 import {CraGeneratorStore} from './app.store';
+import {AllocationByMonth} from './interface/allocation-by-month.interface';
+import {AllocationByDay} from './interface/allocation-by-day.interface';
+import {Agent} from './interface/agent.interface';
 
 describe('CraGeneratorStore', () => {
   let store: any;
@@ -25,7 +28,7 @@ describe('CraGeneratorStore', () => {
   });
 
   it('devrait permettre de sélectionner un agent', () => {
-    store.setCurrentSelectedAgent({ id: 2, name: AgentEnum.AGENT_2, imputationsParMois: [] });
+    store.setCurrentSelectedAgent({ id: 2, name: AgentEnum.AGENT_2, allocationsParMois: [] });
     expect(store.currentSelectedAgentId()).toBe(2);
   });
 
@@ -54,7 +57,7 @@ describe('CraGeneratorStore', () => {
   it('devrait retourner les agents du projet sélectionné', () => {
     const agents = store.getAgentsByCurrentProject();
     expect(agents.length).toBeGreaterThan(0);
-    expect(agents.some((agent: { nom: AgentEnum; }) => agent.nom === AgentEnum.AGENT_1)).toBeTrue();
+    expect(agents.some((agent: Agent) => agent.name === AgentEnum.AGENT_1)).toBeTrue();
   });
 
   it('devrait ajouter et retirer une imputation', () => {
@@ -62,23 +65,23 @@ describe('CraGeneratorStore', () => {
     const dateString = toFullDateString(testDate);
 
     store.toggleDate(testDate);
-    let imputations = store.getImputationsByCurrentAgent().flatMap((imp: { imputations: any; }) => imp.imputations);
-    expect(imputations.some((imp: { date: string; }) => imp.date === dateString)).toBeTrue();
+    let allocations = store.getAllocationsByCurrentAgent().flatMap((imp: AllocationByMonth) => imp.allocationsByDay);
+    expect(allocations.some((imp: AllocationByDay) => imp.date === dateString)).toBeTrue();
 
     store.toggleDate(testDate);
-    imputations = store.getImputationsByCurrentAgent().flatMap((imp: { imputations: any; }) => imp.imputations);
-    expect(imputations.some((imp: { date: string; }) => imp.date === dateString)).toBeFalse();
+    allocations = store.getAllocationsByCurrentAgent().flatMap((imp: AllocationByMonth) => imp.allocationsByDay);
+    expect(allocations.some((imp: AllocationByDay) => imp.date === dateString)).toBeFalse();
   });
 
   it('devrait retourner le bon nombre d’imputations par mois et par type', () => {
     const testDate = new Date('2024-02-20');
     store.toggleDate(testDate);
 
-    const workingDays = store.getNumberOfImputationsDaysByMonthAndType('workingDay', store.currentSelectedAgentId(), '2024-02');
+    const workingDays = store.getNumberOfAllocationsDaysByMonthAndType('workingDay', store.currentSelectedAgentId(), '2024-02');
     expect(workingDays).toBe(1);
 
     store.toggleDate(testDate);
-    const workingDaysAfterRemoval = store.getNumberOfImputationsDaysByMonthAndType('workingDay', store.currentSelectedAgentId(), '2024-02');
+    const workingDaysAfterRemoval = store.getNumberOfAllocationsDaysByMonthAndType('workingDay', store.currentSelectedAgentId(), '2024-02');
     expect(workingDaysAfterRemoval).toBe(0);
   });
 });
